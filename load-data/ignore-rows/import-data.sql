40ab5f23b4831532718d79f57428e083d826c0ed
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-04-25 10:13:56 +0200

    + load-data

---
+++ load-data/ignore-rows/import-data.sql
@@ -0,0 +1,20 @@
+--
+--  "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" --local-infile -u rene -prene tq84_db < import-data.sql
+--
+drop table if exists csv_ignore_rows;
+
+create table csv_ignore_rows (
+   col_1   varchar(10),
+   col_2   varchar(10),
+   col_3   varchar(10)
+);
+
+
+load data local infile 'data.csv'
+   into table csv_ignore_rows
+   fields terminated by ','  enclosed  by '"'
+   lines  terminated by '\r\n'
+   ignore 1 lines
+;
+
+select * from csv_ignore_rows;
