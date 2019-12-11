47d6a813dc64f516ac80eea16323253e84718671
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-12-11 11:04:15 +0100

    + create_table.sql

---
+++ tables/Row-size-too-large_2018/create_table.sql
@@ -0,0 +1,10 @@
+--
+--   "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u rene -prene -Dtq84_db < create_table.sql
+--
+
+drop table if exists problem_8192;
+
+create table problem_8192        (
+  col_01           text
+)-- row_format=compressed
+;
